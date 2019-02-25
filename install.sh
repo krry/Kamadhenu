f3467976255180b4224251eeeff12dbdf74a6825
Kerry Snyder <krry@me.com>
2019-02-25 02:47:21 -0600

    cleaned up, testing install

--- install.sh
+++ install.sh
@@ -4,16 +4,14 @@ if type brew > /dev/null 2>&1; then
     brew update
     brew bundle
     if [ ! -e /usr/local/bin/kamadhenu ] > /dev/null 2>&1 ; then
-        printf "%s\n" "Symlinking in $HOMEBREW_PREFIX/bin"
+        printf "%s\n" "Symlink kamadhenu to $HOMEBREW_PREFIX/bin"
         ln -s "$PWD/kamadhenu" $HOMEBREW_PREFIX/bin/
     fi
-    printf "%s\n" "You may now consult Kamadhenu"
-                \ "Just type kamadhenu"
-                \ "kamadhenu"
-    # KMDIR=$HOMEBREW_PREFIX/share/kamadhenu
-    # echo "Herding cowsays and wrangling figlets..."
-    # cp $PWD/cows $KMDIR
-    # cp $PWD/figlets $KMDIR
-# elif type snap; then
-#     snap install figlet cowsay lolcat fortune
+    echo "Herding cowsays and wrangling figlets..."
+    cp -n $PWD/cows/*.cow $HOMEBREW_PREFIX/share/cows/
+    cp -n $PWD/figlets/*.flf $HOMEBREW_PREFIX/share/figlet/fonts
 fi
+
+(echo "You may now consult Kamadhenu\n"; echo "Just type kamadhenu") | cowsay -s
+figlet -f cola "Kamadhenu" | lolcat -a
+kamadhenu
