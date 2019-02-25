25cc9d490c3a76c82447e2e2b128c2ea63f66aa8
Kerry Snyder <krry@me.com>
2019-02-25 02:10:38 -0600

    rename dirs in accordance with libs

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 printf "%s\n" "Building Kamadhenu's temple"
 
-if type brew >/dev/null; then
+if type brew > /dev/null 2>&1; then
     brew update
     brew bundle
-    if [ ! -e /usr/local/bin/kamadhenu ] ; then
-        printf "%s\n" "Making `kamadhenu` accessible from $PATH"
+    if [ ! -e /usr/local/bin/kamadhenu ] > /dev/null 2>&1 ; then
         printf "%s\n" "Symlinking in $HOMEBREW_PREFIX/bin"
         ln -s "$PWD/kamadhenu" $HOMEBREW_PREFIX/bin/
     fi
+    printf "%s\n" "You may now consult Kamadhenu"
+                \ "Just type kamadhenu"
+                \ "kamadhenu"
     # KMDIR=$HOMEBREW_PREFIX/share/kamadhenu
     # echo "Herding cowsays and wrangling figlets..."
     # cp $PWD/cows $KMDIR
