03d62e1d7a690d7486c3621646b8b3ef783af2ab
Kerry Snyder <krry@me.com>
2019-03-04 06:36:21 -0600

    go local with the install; upcase the Kamadhenu

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,31 +10,34 @@ else
     exit 2
 fi
 
-sudo git clone https://github.com/krry/kamadhenu/ "$BREWFIX/kamadhenu" || \
+sudo git clone https://github.com/krry/Kamadhenu.git || \
 (printf "Cloning the git repository has gone awry." && exit 1)
 
-cd "$BREWFIX/kamadhenu" || (printf "Oops, flubbed the dismount.\nFind \
-'kamadhenu' in %s and type 'brew bundle' to continue.\n" "$BREWFIX" && exit 1)
+cd "./Kamadhenu" || (printf "Oops, flubbed the dismount.\nFind \
+'Kamadhenu' in %s and type 'brew bundle' to continue.\n" "$PWD" && exit 1)
 
 printf "%s\n" "Building Kamadhenu's temple"
 
 if type brew > /dev/null 2>&1; then
     brew update
     brew bundle
-    if [ ! -e /usr/local/bin/kamadhenu ] > /dev/null 2>&1 ; then
-        printf "Symlinking kamadhenu into %s/bin" "$BREWFIX"
-        ln -s "$PWD/kamadhenu" "$BREWFIX/bin/"
+    if [ ! -e /usr/local/bin/Kamadhenu ] > /dev/null 2>&1 ; then
+        printf "Symlinking Kamadhenu into %s/bin\n" "$BREWFIX"
+        ln -s "$PWD/Kamadhenu" "$BREWFIX/bin/"
     fi
     printf "Herding cowsays..."
+    printf "%s\n" "$PWD/cows/*.cow to $BREWFIX/share/cows/"
     cp -n "$PWD/cows/*.cow" "$BREWFIX/share/cows/"
     printf "Wrangling figlets..."
+    printf "%s\n" "$PWD/figlet/fonts/*.flf to $BREWFIX/share/figlet/fonts"
     cp -n "$PWD/figlet/fonts/*.flf" "$BREWFIX/share/figlet/fonts"
     printf "Stuffing fortune cookies..."
+    printf "%s\n" "$PWD/fortunes/* to $BREWFIX/share/games/fortunes"
     cp -n "$PWD/fortunes/*" "$BREWFIX/share/games/fortunes"
 fi
 printf "%s\n" "GREAT SUCCESS!"
 printf "%s\n" "You may now call upon Kamadhenu, like so..." | cowsay -f fox | lolcat
-sleep 2
+sleep 3
 figlet -f computer "Kamadhenu" | lolcat -a
-sleep 1
-kamadhenu
+sleep 3
+Kamadhenu
