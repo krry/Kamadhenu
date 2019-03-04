6a863bb027641f28dccd974c84865e12793b9bf8
Kerry Snyder <krry@me.com>
2019-03-04 06:07:58 -0600

    Debug install script

--- install.sh
+++ install.sh
@@ -3,16 +3,18 @@ printf "Testing for homebrew...\n"
 if type brew > /dev/null 2>&1; then
     printf "Homebrew found. Commencing installation...\n"
     BREWFIX="$(brew --prefix)"
+    printf "You may have to sudo now.\n"
 else
     printf "Sorry, you'll need Homebrew for Kamadhenu to install itself.\n"
     printf "See the REAMDE.md for manual installation instructions.\n"
     exit 2
 fi
 
-git clone https://github.com/krry/kamadhenu/ "$BREWFIX/kamadhenu" || printf
-"Cloning the git repository has gone awry." && exit 1
+sudo git clone https://github.com/krry/kamadhenu/ "$BREWFIX/kamadhenu" || \
+printf "Cloning the git repository has gone awry." && exit 1
 
-cd "$BREWFIX/kamadhenu" || printf "Oops, flubbed the dismount.\nFind 'kamadhenu' in %s and type 'brew bundle' to continue.\n" "$BREWFIX" && exit 1
+cd "$BREWFIX/kamadhenu" || printf "Oops, flubbed the dismount.\nFind \
+'kamadhenu' in %s and type 'brew bundle' to continue.\n" "$BREWFIX" && exit 1
 
 printf "%s\n" "Building Kamadhenu's temple"
 
