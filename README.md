e5f3a2a4862582357ab1f4ff51f01001909b5ad5
Kerry Snyder <krry@me.com>
2019-03-04 05:46:06 -0600

    add fortunes, retool install for bootstrapping, add pic

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 # KAMADHENU
 
 An udderly absurd soothsayer for your shell.
+![The truth shall be our doom](./Nietzsche-cow.png)
 
 ## Installation
 
@@ -13,36 +14,45 @@ cd kamadhenu && . ./install.sh
 ## Usage
 
 Get random insight.
+
+Odds of a repeat: 1 in 275,254,848
+
 ``` sh
 kamadhenu
 ```
 
 Put words in Kamadhenu's mouth?
+
 ``` sh
 kamadhenu I love cheese
 ```
 
 Pipe STDOUT to Kamadhenu like a cat
+
 ``` sh
 curl https://raw.githubusercontent.com/krry/kamadhenu/master/kamadhenu |
 kamadhenu
 ```
 
 Bless yr terminal. Begin each shell sesh with Kamadhenu.
+
 ``` sh
 echo kamadhenu >> .$(basename $SHELL)rc
 ```
 
 ## Dependencies
+
 - [fortune](https://github.com/bmc/fortunes/)
 - [cowsay](https://linux.die.net/man/1/cowsay)
 - [lolcat](https://github.com/busyloop/lolcat)
 - [figlet](http://www.figlet.org/)
 
-I believe each `cowsay`, `fortune`, `figlet`, and `lolcat` are available for install via `brew`, `pip`, `snap`, and `apt-get`, and various other *nix pacmen. You can do eet.
-
+I believe each `cowsay`, `fortune`, `figlet`, and `lolcat` are available for
+install via `brew`, `pip`, `snap`, and `apt-get`, and various other *nix
+pacmen. You can do eet.
 
 ## TODO
+
 - [ ] offer settings mode, a settings.file, and CLI flags
   - cow face settings
   - choose default cow
--- install.sh
+++ install.sh
@@ -1,19 +1,38 @@
+#!/usr/bin/env bash
+printf "Testing for homebrew...\n"
+if type brew > /dev/null 2>&1; then
+    printf "Homebrew found. Commencing installation...\n"
+    BREWFIX="$(brew --prefix)"
+else
+    printf "Sorry, you'll need Homebrew for Kamadhenu to install itself.\n"
+    printf "See the REAMDE.md for manual installation instructions.\n"
+    exit 2
+fi
+
+git clone https://github.com/krry/kamadhenu/ "$BREWFIX/kamadhenu" || printf
+"Cloning the git repository has gone awry." && exit 1
+
+cd "$BREWFIX/kamadhenu" || printf "Oops, flubbed the dismount.\nFind 'kamadhenu' in %s and type 'brew bundle' to continue.\n" "$BREWFIX" && exit 1
+
 printf "%s\n" "Building Kamadhenu's temple"
 
 if type brew > /dev/null 2>&1; then
     brew update
     brew bundle
     if [ ! -e /usr/local/bin/kamadhenu ] > /dev/null 2>&1 ; then
-        printf "%s\n" "Symlink kamadhenu to $HOMEBREW_PREFIX/bin"
-        ln -s "$PWD/kamadhenu" $HOMEBREW_PREFIX/bin/
+        printf "Symlinking kamadhenu into %s/bin" "$BREWFIX"
+        ln -s "$PWD/kamadhenu" "$BREWFIX/bin/"
     fi
-    echo "Herding cowsays and wrangling figlets..."
-    cp -n $PWD/cows/*.cow $HOMEBREW_PREFIX/share/cows/
-    cp -n $PWD/figlet/fonts/*.flf $HOMEBREW_PREFIX/share/figlet/fonts
+    printf "Herding cowsays..."
+    cp -n "$PWD/cows/*.cow" "$BREWFIX/share/cows/"
+    printf "Wrangling figlets..."
+    cp -n "$PWD/figlet/fonts/*.flf" "$BREWFIX/share/figlet/fonts"
+    printf "Stuffing fortune cookies..."
+    cp -n "$PWD/fortunes/*" "$BREWFIX/share/games/fortunes"
 fi
-
-(echo "You may now consult Kamadhenu\n"; echo "Just type kamadhenu") | cowsay -s
+printf "%s\n" "GREAT SUCCESS!"
+printf "%s\n" "You may now call upon Kamadhenu, like so..." | cowsay -f fox | lolcat
 sleep 2
-figlet -f basic "Kamadhenu" | lolcat -a
+figlet -f computer "Kamadhenu" | lolcat -a
 sleep 1
 kamadhenu
