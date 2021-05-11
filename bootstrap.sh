edd609681ee4af8225854cf860a3162130615d11
Kerry Snyder <kerry@atmanaut.me>
2021-05-11 01:21:48 -0700

    prettier, numbier, slower, less broken

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-# Toolset
+# CONSTANTS
+BREWFIX="$(brew --prefix)"
+COW_DIR=${BREWFIX}/share/cows/
+FIGLET_DIR=${BREWFIX}/share/figlet/fonts
+FORTUNE_DIR=${BREWFIX}/share/games/fortunes
+
+# UTILS
 hr_msg ()  {
     echo ''
     printf -v _hr "%*s" "$(tput cols)" ""
@@ -30,47 +36,47 @@ failure () {
     exit
 }
 
-# Checking for Homebrew
-if type brew > /dev/null 2>&1; then
-    message "Homebrew found. Commencing installation..."
-    message "Cloning the Kamadhenu repository..."
-else
-    echo ''
-    printf "Sorry, you'll need Homebrew for Kamadhenu to install itself."
-    printf "See the REAMDE.md for manual installation instructions."
-    exit 2
-fi
-
-# CONSTANTS
-BREWFIX="$(brew --prefix)"
-COW_DIR=${BREWFIX}/share/cows/
-FIGLET_DIR=${BREWFIX}/share/figlet/fonts
-FORTUNE_DIR=${BREWFIX}/share/games/fortunes
-
-# Cloning
-git clone --quiet https://github.com/krry/Kamadhenu.git
-git_clone_status=$?
-
-if (($git_clone_status < 2)); then
-    success "Repository cloned into $PWD"
-    cd "./Kamadhenu" && cd_status=$?
-    if ((cd_status < 2)); then
-        message "Updating brew and unbundling brewfile..."
-        brew_up
+# RUNTIME
+check_for_brew () {
+    # Checking for Homebrew
+    if type brew > /dev/null 2>&1; then
+        message "Homebrew found. Commencing installation..."
+        message "Cloning the Kamadhenu repository..."
+        clone_kama
     else
-        failure "Oops, flubbed the dismount. Couldn't find the cloned files."
-        failure "Find 'Kamadhenu' in $PWD and type 'brew bundle' to continue."
+        failure "Sorry, you'll need Homebrew for Kamadhenu to install itself."
+        hr_msg "See the REAMDE.md for manual installation instructions."
+        exit 2
+    fi
+}
+
+clone_kama () {
+    # Cloning
+    git clone --quiet https://github.com/krry/Kamadhenu.git
+    git_clone_status=$?
+
+    if (($git_clone_status < 2)); then
+        success "Repository cloned into $PWD"
+        cd "./Kamadhenu" && cd_status=$?
+        if ((cd_status < 2)); then
+            message "Updating brew and unbundling Brewfile..."
+            echo ''
+            brew_up
+        else
+            failure "Oops, flubbed the dismount. Couldn't find the cloned files."
+            failure "Find 'Kamadhenu' in $PWD and type 'brew bundle' to continue."
+            exit 1
+        fi
+    else
+        failure "Cloning the git repository has gone awry."
         exit 1
     fi
-else
-    failure "Cloning the git repository has gone awry."
-    exit 1
-fi
+}
 
 brew_up () {
-    # Brewing
     brew update && brew bundle
     brew_status=$?
+
     if (($brew_status < 2)); then
         success "Brewed and ready."
         build_temple
@@ -94,11 +100,12 @@ build_temple () {
 }
 
 herd_cowsays () {
-    # Copying Data
     hr_msg "Herding cowsays" "(oo) "
+
     cp -n "${PWD}/cows/"*.cow "$COW_DIR"
-    cow_status=$?
-    if (($cow_status < 2)); then
+    cowsay_status=$?
+
+    if (($cowsay_status < 2)); then
         success "Cowsays herded into $COW_DIR"
     else
         warning "Couldn't copy all the cows into $COW_DIR"
@@ -139,21 +146,18 @@ finish_install () {
         warning "Check the warnings above to fix the flubs."
     else
         success "Installation complete."
-        echo ''
         hr_msg "GREAT SUCCESS!" "$"
         sleep 2
         echo ''
-        echo ''
-        printf "You may now call upon Kamadhenu, like so..." | cowsay -f fox | lolcat
-        echo ''
+        printf "You may now call upon Kamadhenu by name." | cowsay -f fox | lolcat
         echo ''
         sleep 3
         echo ''
-        echo ''
-        figlet -f computer "Kamadhenu" | lolcat -a
-        echo ''
+        figlet -f jazmine "Kamadhenu" | lolcat -a
         echo ''
         sleep 3
         Kamadhenu
     fi
 }
+
+check_for_brew
